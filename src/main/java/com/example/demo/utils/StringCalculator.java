ae7ffc442524dc59c58382e0533f72c93863b833
Tushar515 <tmulik519@gmail.com>
2021-06-19 18:51:02 +0530

    Fourth Commit

--- src/main/java/com/example/demo/utils/StringCalculator.java
+++ src/main/java/com/example/demo/utils/StringCalculator.java
@@ -6,15 +6,22 @@ import org.springframework.stereotype.Service;
 public class StringCalculator {
 
     public int Add(String numbers) {
+        int add = 0;
 
+        // Handle an empty string
         if(numbers.isEmpty())
             return 0;
         String[] splittedNumbers = numbers.split(",");
 
+        // Handle only one number
         if(splittedNumbers.length == 1)
             return Integer.parseInt(splittedNumbers[0]);
 
-        return Integer.parseInt(splittedNumbers[0]) +
-                Integer.parseInt(splittedNumbers[1]);
+        // Handle Unknown Amount of Numbers
+        for(String s : splittedNumbers){
+            add += Integer.parseInt(s);
+        }
+
+        return add;
     }
 }
--- src/test/java/com/example/demo/utils/DemoApplicationTests.java
+++ src/test/java/com/example/demo/utils/DemoApplicationTests.java
@@ -1,15 +1,16 @@
 package com.example.demo.utils;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
-
 	StringCalculator stringCalculator;
 
 	@BeforeEach
@@ -18,14 +19,36 @@ class DemoApplicationTests {
 	}
 
 	@Test
-	public void Add(){
+	@DisplayName("AddUnknownAmountOfNumbers")
+	void addUnknownAmountOfNumbers(){
 		// Given
-		String test = "1,2";
+		String test = "1,2,3,4,5,6,7,8,9,10";
 		// When
 		int isValid = stringCalculator.Add(test);
-		System.out.println(isValid);
 		// Then
-		assertThat(isValid).isPositive();
+		assertThat(isValid).isEqualTo(55);
+	}
+
+	@Test
+	@DisplayName("returnOnlySingleNumber")
+	void returnOnlySingleNumber(){
+		// Given
+		String test = "1";
+		// When
+		int isValid = stringCalculator.Add(test);
+		// Then
+		assertThat(isValid).isEqualTo(1);
+	}
+
+	@Test
+	@DisplayName("returnZero")
+	void returnZero(){
+		// Given
+		String test = "";
+		// When
+		int isValid = stringCalculator.Add(test);
+		// Then
+		assertThat(isValid).isEqualTo(0);
 	}
 
 }
